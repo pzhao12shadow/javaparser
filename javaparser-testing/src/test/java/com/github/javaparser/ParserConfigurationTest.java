bd98a423d10f700c1635eb94cba6bd6acce913cd
pzhao12 <pzhao12@vlad>
2018-02-11 15:37:12 -0600

    1cca4c46e53b5a89f5276af30fe0971025e04607 replay gib_eks

--- javaparser-testing/src/test/java/com/github/javaparser/ParserConfigurationTest.java
+++ javaparser-testing/src/test/java/com/github/javaparser/ParserConfigurationTest.java
@@ -3,7 +3,6 @@ package com.github.javaparser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.stmt.Statement;
-import com.github.javaparser.ast.validator.NoProblemsValidator;
 import org.junit.Test;
 
 import static com.github.javaparser.ParseStart.STATEMENT;
@@ -18,14 +17,14 @@ public class ParserConfigurationTest {
     public void storeNoTokens() {
         ParseResult<CompilationUnit> result = new JavaParser(new ParserConfiguration().setStoreTokens(false)).parse(ParseStart.COMPILATION_UNIT, provider("class X{}"));
 
-        assertFalse(result.getTokens().isPresent());
+        assertFalse(result.getResult().get().getTokenRange().isPresent());
         assertTrue(result.getResult().get().findAll(Node.class).stream().noneMatch(node -> node.getTokenRange().isPresent()));
     }
 
     @Test
     public void noProblemsHere() {
         ParseResult<Statement> result =
-                new JavaParser(new ParserConfiguration().setLanguageLevel(ANY))
+                new JavaParser(new ParserConfiguration().setLanguageLevel(RAW))
                         .parse(STATEMENT, provider("try{}"));
         assertEquals(true, result.isSuccessful());
     }
